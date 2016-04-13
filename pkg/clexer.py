2954562e54cf071ca09035667c87aec3710735e8
myyang <ymy1019@gmail.com>
2016-04-13 12:08:15 +0800

    Fix custom lexer inheritance

--- pkg/clexer.py
+++ pkg/clexer.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re
-
+from pygments import lexer
 from pygments import lexers
 
 from . import ctoken
 
+
 class BSGHtmlLexer(lexers.HtmlLexer):
 
-    def __init__(self, **options):
-        super(BSGHtmlLexer, self).__init__(**options)
-
-        # see: pygments/lexer.py #419
-        # XXX: we are using inproper way to extend here...
-        self._tokens['root'].insert(3, (re.compile(r'</*bsg-ht>').match, ctoken.BSG_HT, None))
+    tokens = {
+        'root': [
+            # BSG custom tags.
+            (r'</*bsg-ht>', ctoken.BSG_HT),
+            # inherit
+            lexer.inherit,
+        ],
+    }
