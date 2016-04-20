940b9dda83c2721b31c4a3118a3f739332fd93da
myyang <ymy1019@gmail.com>
2016-04-21 01:03:32 +0800

    Add css select function

--- bsg/pkg/soup.py
+++ bsg/pkg/soup.py
@@ -12,6 +12,8 @@ from . import clexer, cformatter
 
 class BSG(object):
 
+    css_ht_tag = 'bsg-css-ht'
+
     def __init__(self, html):
         self.soup = BeautifulSoup(html, 'html.parser')
 
@@ -23,7 +25,6 @@ class BSG(object):
 
         for t in tags:
             t.wrap(self.soup.new_tag('bsg-ht'))
-        # Garfield: apply pipeline to find other types like content, class, id, url ...
 
         html_lexer = clexer.BSGHtmlLexer()
         html_formatter = cformatter.BSGHtmlFormatter()
@@ -32,4 +33,27 @@ class BSG(object):
         highlight_html = re.sub("<bsg-ht>\s*\n", "<bsg-ht>", highlight_html)
         highlight_html = re.sub(">[\s\n]*</bsg-ht>", "></bsg-ht>", highlight_html)
 
+        return highlight_html
+
+    def highlight_by_css(self, css_str):
+        tags = self.soup.select(css_str)
+
+        if not tags:
+            return self.soup.prettify()
+
+        return self._highlight(tags, self.css_ht_tag)
+
+    def _highlight(self, tags, wrap_str):
+        for t in tags:
+            t.wrap(self.soup.new_tag(wrap_str))
+
+        html_lexer = clexer.BSGHtmlLexer()
+        html_formatter = cformatter.BSGHtmlFormatter()
+        highlight_html = highlight(self.soup.prettify(), html_lexer, html_formatter)
+
+        wrap_tag, wrap_tag_end = "<{}>".format(wrap_str), "</{}>".format(wrap_str)
+        highlight_html = re.sub(
+            r">[\s\n]*{}".format(wrap_tag_end), ">"+wrap_tag_end,
+            re.sub(r"{}\s*\n".format(wrap_str), wrap_tag, highlight_html)
+        )
         return highlight_html
